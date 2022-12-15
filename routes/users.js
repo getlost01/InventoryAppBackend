7394ddd31a2282dd010f200c04e6be8a944e8001
Getlost01 <aagamjain573@gmail.com>
2022-12-15 10:37:51 +0530

    final testing1

--- routes/users.js
+++ routes/users.js
@@ -61,10 +61,8 @@ router.get("/:id/verify/:token/", async (req, res) => {
 			token: req.params.token,
 		});
 		if (!token) return res.status(400).send({ message: "Invalid link" });
-
-		await User.updateOne({ _id: user._id, verified: true });
+		await User.updateOne({ _id: user._id},{ verified: true });
 		await token.remove();
-
 		res.status(200).send({ message: "Email verified successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
