defc732008303241ec3be08e42157cd64e6f40e8
Aagam Jain <79409258+getlost01@users.noreply.github.com>
2023-06-10 09:59:02 +0530

    add wakeup & 404 endpoints

--- index.js
+++ index.js
@@ -24,5 +24,25 @@ app.use("/api/auth", authRoutes);
 app.use("/api/password-reset", passwordResetRoutes);
 app.use("/api/product", ProductRoutes)
 
+app.get("/wake/api", (req, res) => {
+    res.status(200).json({ success: true, message: "API is awake" });
+});
 
+app.get("*", (req, res) => {
+    res.status(404).json({ success: false, message: "Page not found 404", endPoints: {
+        "login": "/api/auth/login",
+        "register": "/api/auth/register",
+        "forgot-password": "/api/password-reset/forgot-password",
+        "reset-password": "/api/password-reset/reset-password",
+        "get-all-users": "/api/users",
+        "get-user-by-id": "/api/users/:id",
+        "update-user-by-id": "/api/users/:id",
+        "delete-user-by-id": "/api/users/:id",
+        "get-all-products": "/api/product",
+        "get-product-by-id": "/api/product/:id",
+        "update-product-by-id": "/api/product/:id",
+        "delete-product-by-id": "/api/product/:id",
+        "create-product": "/api/product",
+    } });
+});
 
